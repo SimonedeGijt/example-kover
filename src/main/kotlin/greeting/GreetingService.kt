5b896c866b70683da55354c3a002ad83a7cbdc34
Simone de Gijt <s.degijt@quin.md>
2022-05-14 12:05:16 +0200

    :art:

--- src/main/kotlin/greeting/GreetingService.kt
+++ src/main/kotlin/greeting/GreetingService.kt
@@ -4,17 +4,18 @@ import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
+    private final inline fun inlineFunction(myFun: () -> String) = myFun()
+
     fun standardGreet() = inlineFunction {
         val greeting = "Hello world"
         println(greeting)
         return@inlineFunction greeting
     }
 
-    fun greet(s: String?) = if (s.isNullOrEmpty()) standardGreet() else inlineFunction {
-        val greeting = "Hello $s"
-        println(greeting)
-        return@inlineFunction greeting
-    }
-
-    private final inline fun inlineFunction(myFun: () -> String) = myFun()
+    fun greet(s: String?) = if (s.isNullOrEmpty()) standardGreet() else
+        inlineFunction {
+            val greeting = "Hello $s"
+            println(greeting)
+            return@inlineFunction greeting
+        }
 }
